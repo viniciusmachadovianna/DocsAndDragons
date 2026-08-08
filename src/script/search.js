90eb1ed0d1f749e41981fb8a386f54d03ee712c3
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2026-08-08 17:50:54 -0300

    feat: add quick category filters

--- src/script/search.js
+++ src/script/search.js
@@ -1,15 +1,84 @@
+/**
+ * @fileoverview
+ *
+ */
 
+// --- CONFIG --- //
 const DOM = {
     searchInput: document.getElementById("keyword"),
     codex: document.getElementById("codex"),
 }
 
+// --- MAIN --- //
+
+let activeFilters = ["class", "equipment", "spell"]
+
+async function loadCategoryItems(category) {
+
+    let origin = `/data/${category}.json`;
+    const response = await fetch(origin);
+    const items = await response.json();
+    
+    return items;
+}
+
+async function renderList() {
+    
+    const codex = document.getElementById('codex');
+    codex.innerHTML = '';
+    
+    for (const category of activeFilters) {
+        
+        const categoryItemsInfo =  await loadCategoryItems(category);
+        
+        for (const itemInfo of categoryItemsInfo) {
+            let card = `<li> ${itemInfo.name} | ${itemInfo.codexCategory}</li>`;
+            codex.innerHTML += card;
+        }
+    }
+}
+
+renderList();
+
+function filterItems(searchKeyword, categories=null){
+
+  let searchTerm = normalizeText(searchKeyword);
+}
+
+// --- AUX --- //
+
+function changeFilters(tgt){
+    if (tgt.checked) {
+      op = 'Adicionando';
+      activeFilters.push(tgt.value)
+    }
+    else{
+      op = 'Removendo';
+      activeFilters.splice(activeFilters.indexOf(tgt.value), 1);
+    }
+    console.log(`${op} '${tgt.value}' à lista de categorias filtradas`);
+    renderList();
+}
+
 function normalizeText(txt) {
+  
   return txt
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
 }
 
-let searchKeyword = DOM.searchInput.value;
-// DOM.searchInput.addEventListener('input', handleSearchInput(searchKeyword))
+// --- HANDLERS --- //
+
+const searchFilters = document.getElementById('searchbar').querySelectorAll('input[type=checkbox')
+
+searchFilters.forEach(filter=> {
+  let op = '';
+  filter.addEventListener('change', (event) => changeFilters(event.target));
+});
+
+DOM.searchInput.addEventListener('input', (event) => {
+  let searchKeyword = event.target.value;
+  filterItems(searchKeyword);
+});
+;
